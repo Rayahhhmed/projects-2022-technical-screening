556e3405a6138e2d3a048cb75cf06c415ef7ef26
Rayahhhmed <raiyan.ahmed@gmail.com>
2022-01-13 02:21:47 +1100

    Save for the day for hard.py
    Note to self: Change the json path to stop errno

--- hard.py
+++ hard.py
@@ -17,9 +17,15 @@ A good solution is favourable but does not guarantee a spot in Projects because
 we will also consider many other criteria.
 """
 import json
+import re 
 
+MONOSPACE = ' '
+COURSE_CODE_RANGE = 4
+LOGIC_OPERATIONS = ["AND", "OR"]
+LB = '('
+RB = ')'
 # NOTE: DO NOT EDIT conditions.json
-with open("./conditions.json") as f:
+with open("screening_for_subcom\projects-2022-technical-screening\conditions.json") as f:
     CONDITIONS = json.load(f)
     f.close()
 
@@ -30,15 +36,86 @@ def is_unlocked(courses_list, target_course):
     You do not have to do any error checking on the inputs and can assume that
     the target_course always exists inside conditions.json
 
-    You can assume all courses are worth 6 units of credit
-    """
+    You can assume all courses are worth 6 units of credit"""
+
+    """The design for checking if a person has done a particular source would be done with
+    lists. Think of it like a decision tree."""
+
+    unparsed_string = CONDITIONS
+    # need to check if there were no courses inputted 
+    # not sure if there would be a case where target course not given
+    handle_trivial_cases(courses_list, target_course)
+    print(CONDITIONS[target_course].split())
+     
+    # delimit string to list 
+    monospaced_prerequisites = MONOSPACE.join(CONDITIONS[target_course].split())
+    if len(monospaced_prerequisites) == 1:
+        return monospaced_prerequisites in courses_list
     
-    # TODO: COMPLETE THIS FUNCTION!!!
+    construct_course_progressions(0, monospaced_prerequisites)
     
-    return True
+
+
+
+def handle_trivial_cases(courses_list, target_course):    
+    """need to amortise for trivial cases"""
+    if len(courses_list) == 0:
+        return False
+
+def check_if_valid_course_name(course_name):
+    """ This will check for the last 4 digits of a course name"""
+    course_is_valid = True
+    name_len = len(course_name)
+    for x in range(1, COURSE_CODE_RANGE):
+        if course_name[name_len - x].isnumeric() is False: 
+            course_is_valid = False 
+    return course_is_valid
+
+
+def check_subsequent_course_key_with_bracket(lo, monospaced_prerequisites):
+    count_brackets = 0
+    temp_list = []
+    for x in range(lo, len(monospaced_prerequisites) - 1):
+        count_brackets += monospaced_prerequisites[x].count(LB)
+        count_brackets -= monospaced_prerequisites[x].count(RB)
+        monospaced_prerequisites[x].split()
+        monospaced_prerequisites[x].remove(LB)
+        monospaced_prerequisites[x].remove(RB)
+        temp_list.append(monospaced_prerequisites[x])
+        if count_brackets is 0:
+            return temp_list
+        
+
+def construct_course_progressions(lo, monospaced_prerequisites):
+
+    for index in range(lo, len(monospaced_prerequisites) - 1):
+        course_name = monospaced_prerequisites[index]
+        if LB or RB in course_name:
+            check_subsequent_course_key_with_bracket(index, monospaced_prerequisites)
+        # Check if the thing has The operators or course codes
+        if course_name in LOGIC_OPERATIONS: 
+            if course_name is "AND":
+                "check if previous course name is a list or string"
+               # if type(monospaced_prerequisites[index - 1]) is str: 
+                pass
+            else: 
+                "the code here is an \"OR\" "
+                monospaced_prerequisites.remove(course_name)
+
+                if type(monospaced_prerequisites[index - 1]) is str:
+                    temp_list = []
+                    temp_list.append(monospaced_prerequisites[index - 1])
+                    temp_list.append(monospaced_prerequisites[index])
+                    monospaced_prerequisites[index - 1] = temp_list
+                else: 
+                     monospaced_prerequisites[index - 1].append(course_name) 
+        elif check_if_valid_course_name(course_name) == False:
+            print("Course name bad!")
+            
 
 
 
 
+if __name__ == '__main__':  
+    print(is_unlocked(["COMP1531 AND (COMP2521 OR COMP1927)"], "COMP2511"))
 
-    
